3ad2b22b188d53da5d7b1c1fbfadd718c1959e8d
iasha3 <vira23smailauk@gmail.com>
2023-04-13 16:51:44 +0300

    new

---
+++ JWT_token_api/Dto/UserDto.cs
@@ -0,0 +1,8 @@
+namespace JWT_token_api.Dto
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ JWT_token_api/Models/User.cs
@@ -0,0 +1,9 @@
+namespace JWT_token_api.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty; 
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
